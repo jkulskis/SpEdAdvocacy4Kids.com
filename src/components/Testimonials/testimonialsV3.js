4f35b0643d26384317dc4fe445a5ecba5cab7888
John Mikulskis <jkulskis@bu.edu>
2022-04-27 10:28:06 -0400

    new testimonial

--- src/components/Testimonials/testimonialsV3.js
+++ src/components/Testimonials/testimonialsV3.js
@@ -67,19 +67,18 @@ export default function testimonialsV3() {
           <div className="testimonial">
             <p className="font-weight-normal dark-grey-text">
               <i className="blockquote pr-2">
-                We were having difficulty with our daughter in the Belmont
-                Public Schools.  Margie used her advocacy skills to ensure our
-                daughter get the correct services.  She helped us to find the
-                right expert to evaluate our daughter and obtain an IEP from the
-                school.  She worked with our Team to build attainable IEP
-                goals.  She introduced us to many useful resources.  Margie is
-                an inspriation to me.  She really cares.  It isn't just a job
-                for her.
+                I cannot say enough good things about Margie and her dedication
+                to the students she works with and their families! I recently
+                worked with her on a difficult case that would not have yielded
+                such a positive result if not for her hard work. It was lovely
+                to partner with such a professional, compassionate, and
+                knowledgable individual. I will definitely refer her to
+                applicants that are not eligible for our services in that I know
+                they will be in great hands.
               </i>
             </p>
-            <h5 className="font-weight-bold blue-text my-3">
-              -- Belmont High School Parent
-            </h5>
+            <h5 className="font-weight-bold blue-text my-3">-- Attorney Karen Michel</h5>
+            <h6 className="blue-text my-3">Legal Services Fellow at South Coast Counties Legal Services, Inc</h6>
           </div>
         </div>
         <div className="col-md-4 mb-md-0 mb-5 mx-auto">
@@ -95,6 +94,27 @@ export default function testimonialsV3() {
             </p>
             <h5 className="font-weight-bold blue-text my-3">-- Kim Nguyen</h5>
             <h6 className="blue-text my-3">Brockton Parent</h6>
+          </div>
+        </div>
+      </div>
+      <div className="row text-center pb-5">
+        <div className="col-md-4 mb-md-0 mb-5 mx-auto">
+          <div className="testimonial">
+            <p className="font-weight-normal dark-grey-text">
+              <i className="blockquote pr-2">
+                We were having difficulty with our daughter in the Belmont
+                Public Schools.  Margie used her advocacy skills to ensure our
+                daughter get the correct services.  She helped us to find the
+                right expert to evaluate our daughter and obtain an IEP from the
+                school.  She worked with our Team to build attainable IEP
+                goals.  She introduced us to many useful resources.  Margie is
+                an inspriation to me.  She really cares.  It isn't just a job
+                for her.
+              </i>
+            </p>
+            <h5 className="font-weight-bold blue-text my-3">
+              -- Belmont High School Parent
+            </h5>
           </div>
         </div>
       </div>
